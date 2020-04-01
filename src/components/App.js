9c3d320058aaeb78b547dd8873ff09c3b5d9b3d2
Anushka Verma <v.anushka786@gmail.com>
2020-04-01 17:16:35 +0530

    add streams setup

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 
 import Header from "./layout/Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 import history from "../history";
 
 const App = () => {
@@ -10,7 +15,18 @@ const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Switch></Switch>
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route
+              path="/streams/edit/:id"
+              exact
+              component={StreamEdit}
+              StreamEdit
+            />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
